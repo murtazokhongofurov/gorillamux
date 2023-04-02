0348bb4b682f9647c4cd49a709e4b4f6e4dbc015
Murtazokhon <104910848+murtazokhongofurov@users.noreply.github.com>
2023-04-02 21:52:05 +0500

    first commit
    create new main func

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type User struct {
+	ID       string `json:"id"`
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
