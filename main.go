13311fa272ca317fa3b382fc8aec75822c54a9f6
Murtazokhon <104910848+murtazokhongofurov@users.noreply.github.com>
2023-04-02 21:57:39 +0500

    add two router method create and get
    add methods

--- main.go
+++ main.go
@@ -34,3 +34,34 @@ func main() {
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
 
+
+
+// createUser creates a new user
+func createUser(w http.ResponseWriter, r *http.Request) {
+	var user User
+	json.NewDecoder(r.Body).Decode(&user)
+	users = append(users, user)
+	fmt.Println(users)
+	json.NewEncoder(w).Encode(user)
+}
+
+// getUsers gets all users
+func getUsers(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(users)
+}
+
+// getUser gets a single user by ID
+func getUser(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+
+	for _, user := range users {
+		if user.ID == params["id"] {
+			json.NewEncoder(w).Encode(user)
+			return
+		}
+	}
+
+	json.NewEncoder(w).Encode(&User{})
+}
+
+
